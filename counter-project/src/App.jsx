420d129fedae69bbc6c3d7c42d1071dcbee083f2
Harsh <harshbhagat583@gmail.com>
2024-12-20 16:57:05 +0530

    interviev Question on counter

--- counter-project/src/App.jsx
+++ counter-project/src/App.jsx
@@ -9,9 +9,20 @@ function App() {
   //let counter = 5;
 
   const addValue = () => {
-    console.log("clicked",counter);
-    if(counter!=20) counter++;
-    setCounter(counter) 
+    // console.log("clicked",counter);
+    // if(counter!=20) counter++;
+    // setCounter(counter+1) 
+    // setCounter(counter+1) 
+    // setCounter(counter+1) // inn sab ko mila k ui m bss counter +1 he hoga bcz ye pura ek batch bnn jaiga nd java script dekhega are yha to ek he kaam ho rha hai
+    // setCounter(counter+1) 
+    // setCounter(counter+1) 
+    // setCounter(counter+1) 
+
+    // setCounter((prevCounter) => prevCounter+1) 
+    // setCounter((prevCounter) => prevCounter+1) // yha call back hua h to yha counter ek saath jitna call back h utna badhega kyu ki yha previous state le rhe h to ek se prevCount badhega then dusre k pas jaiga yha ek bunch nhi ho rha 
+    // setCounter((prevCounter) => prevCounter+1) 
+    // setCounter((prevCounter) => prevCounter+1) 
+
   }
   const removevalue = () => {
     console.log("clicked",counter);
