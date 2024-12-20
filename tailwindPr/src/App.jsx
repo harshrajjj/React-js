4e47bc2cb768a84510cb577f729147ee1e54201e
Harsh <harshbhagat583@gmail.com>
2024-12-20 16:42:04 +0530

    tailwind

---
+++ tailwindPr/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import Card from "./component/Card"
+
+function App() {
+  const [count, setCount] = useState(0)
+   let myobject = {
+    username:"harsh",
+    age:21
+   }
+
+   let newArr = [1,2,3]
+  return (
+    <>
+      <h1 className='bg-green-500 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card   someObject={myobject} />
+      <Card/>
+    </>
+  )
+}
+
+export default App
