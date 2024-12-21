302479c8e699eb87fdfa4b3ce0b0b6dbae84c993
Harsh <harshbhagat583@gmail.com>
2024-12-21 17:34:13 +0530

    name changes

--- tailwindProject/src/App.jsx
+++ tailwindProject/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <>
       <h1 className='bg-green-500 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
       <Card   someObject={myobject} />
+      <br/>
       <Card/>
     </>
   )
