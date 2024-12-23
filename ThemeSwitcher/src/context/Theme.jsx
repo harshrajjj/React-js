e9f817fc6c2b72ea02eaa0f9505e46aa98c62850
Harsh <harshbhagat583@gmail.com>
2024-12-24 00:00:45 +0530

    commented

--- ThemeSwitcher/src/context/Theme.jsx
+++ ThemeSwitcher/src/context/Theme.jsx
@@ -1,7 +1,7 @@
 import React,{createContext,useContext} from 'react'
 
 
-// peechele wale project m hum value nhi daal rhe the ceeat context m but create context m default value dali jaa sakti hai phly v hum new file bna k hum export kar rhe the yha hum isi file se export kar rhe hai ye default variable daal rhe hai 
+// peechele wale project m hum value nhi daal rhe the ceeat context m but create context m default value dali jaa sakti hai phly v hum new file bna k hum export kar rhe the yha hum isi file se export kar rhe hai ye default variable daal rhe hai ye default valur error se bachaig but ye tareeka static hai 
 export const ThemeContext = createContext({
     themeMode:'light',
     darkTheme: () => {
