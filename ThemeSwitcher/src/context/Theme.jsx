c90d35d4dd6508b20542567a1102b6eb39be6c1b
Harsh <harshbhagat583@gmail.com>
2024-12-23 20:03:17 +0530

    Theme Switcher

---
+++ ThemeSwitcher/src/context/Theme.jsx
@@ -0,0 +1,26 @@
+import React,{createContext,useContext} from 'react'
+
+
+// peechele wale project m hum value nhi daal rhe the ceeat context m but create context m default value dali jaa sakti hai phly v hum new file bna k hum export kar rhe the yha hum isi file se export kar rhe hai ye default variable daal rhe hai 
+export const ThemeContext = createContext({
+    themeMode:'light',
+    darkTheme: () => {
+
+    },
+    lightTheme: () => {
+
+    },
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+
+//custom hook
+export default function useTheme () {
+        return useContext(ThemeContext)
+}
+
+
+
+
+
