06e49687986bdaf340cf589f8eb894aa9e4c443a
Harsh <harshbhagat583@gmail.com>
2024-12-25 20:23:00 +0530

    edit code formatting

--- ReduxToolkit/src/Store/store.js
+++ ReduxToolkit/src/Store/store.js
@@ -1,8 +1,9 @@
-import {configureStore} from "@reduxjs/toolkit" // for configure store jiske andar object jta hai
-import todoReducer from "../features/todo/todoSlices"
+import { configureStore } from "@reduxjs/toolkit"; // for configure store jiske andar object jta hai
+import todoReducer from "../features/todo/todoSlices";
 
-// yha pe jo jo va reducer register krenge usi se ye store value le k update krega 
+// yha pe jo jo va reducer register krenge usi se ye store value le k update krega
 
-export const store = configureStore({ // yha p sare reducer ka list dale jo hum le k aai hai todoslice se// yha jo name die hai create slice m usi naam se redicer aaiga i.e. nameReducer
-    reducer:todoReducer// iske andar sare reducer introduce karna hoga  kyu ki isko btana hota hai ki sare reducer kha p hai
-})
+export const store = configureStore({
+  // yha p sare reducer ka list dale jo hum le k aai hai todoslice se// yha jo name die hai create slice m usi naam se redicer aaiga i.e. nameReducer
+  reducer: todoReducer, // iske andar sare reducer introduce karna hoga  kyu ki isko btana hota hai ki sare reducer kha p hai
+});
